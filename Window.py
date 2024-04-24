12f0d64ab025b28aaa89d90bdf4ce8ff1d83cf1c
Felix Umland <futc@tuta.io>
2024-04-24 16:48:01 +0200

    milestone 1 ez pz

--- Window.py
+++ Window.py
@@ -3,11 +3,12 @@
 
 """
 Window System - Submission
-by  Student Name 1 (#999999)
-and Student Name 2 (#999999)
+by Felix Umland (#406886)
+and Jannick Brändel (#405391)
 """
 
 from GraphicsEventSystem import *
+
 
 class Window:
     def __init__(self, originX, originY, width, height, identifier):
@@ -17,41 +18,66 @@ class Window:
         self.height = height
         self.identifier = identifier
         self.backgroundColor = COLOR_LIGHT_GRAY
-        
+
         self.childWindows = []
         self.parentWindow = None
-    
 
-
-        
     def addChildWindow(self, window):
-        pass
-        
+        self.childWindows.append(window)
+        window.parentWindow = self
+
     def removeFromParentWindow(self):
-        pass
-        
+        self.parentWindow.childWindows.remove(self)
+
     def childWindowAtLocation(self, x, y):
+        for i in reversed(range(len(self.childWindows))):
+            child = self.childWindows[i]
+            print("testing: " + child.identifier)
+            if child.hitTest(x - child.x, y - child.y):
+                print("hit: " + child.identifier)
+                print("x: " + str(x - child.x) + " y: " + str(y - child.y))
+                if len(child.childWindows) > 0:
+                    return child.childWindowAtLocation(x - child.x, y - child.y)
+                else:
+                    return child
         return None
-    
+
     def hitTest(self, x, y):
-        return False
-    
+        if x < 0 or y < 0:
+            return False
+        else:
+            return x < self.width and y < self.height
+
     def convertPositionToScreen(self, x, y):
-        return (0,0)
-    
+        totalX = x
+        totalY = y
+        parent = self.parentWindow
+
+        while parent is not None:
+            totalX += parent.x
+            totalY += parent.y
+            parent = parent.parentWindow
+
+        return totalX, totalY
+
     def convertPositionFromScreen(self, x, y):
-        return (0,0)
-    
-    
+        localX = x - self.convertPositionToScreen(x, y)[0]
+        localY = y - self.convertPositionToScreen(x, y)[1]
+        return localX, localY
+
     def draw(self, ctx):
-        pass
-    
-    
-    
+        position = self.convertPositionToScreen(self.x, self.y)
+        ctx.setOrigin(position[0], position[1])
+        ctx.setFillColor(self.backgroundColor)
+        ctx.fillRect(0, 0, self.width, self.height)
+        for child in self.childWindows:
+            child.draw(ctx)
+
     def handleMouseClicked(self, x, y):
         print("Window " + self.identifier + " was clicked.")
-        
 
+    def setBackgroundColor(self, color):
+        self.backgroundColor = color
 
 
 class Screen(Window):
@@ -59,7 +85,5 @@ class Screen(Window):
         super().__init__(0, 0, windowSystem.width, windowSystem.height, "SCREEN_1")
         self.windowSystem = windowSystem
 
-        
     def draw(self, ctx):
         super().draw(ctx)
-    
--- WindowSystem.py
+++ WindowSystem.py
@@ -7,23 +7,55 @@ by  Student Name 1 (#999999)
 and Student Name 2 (#999999)
 """
 
-from GraphicsEventSystem import *
+import GraphicsEventSystem
 from Window import *
 
 class WindowSystem(GraphicsEventSystem):
     def start(self):
-        pass
-    
+        # add screen
+        self.screen = Screen(self)
+
+        # temporarily save mouse down events
+        self.tempMouseDown = (0, 0)
+        self.mouseClickTolerance = 2
+
+        # add a few test windows
+        window1 = Window(20, 20, 80, 80, "1")
+        window1.setBackgroundColor(COLOR_GREEN)
+        window2 = Window(40, 40, 80, 80, "2")
+        window2.setBackgroundColor(COLOR_LIGHT_BLUE)
+        window3 = Window(20, 20, 40, 40, "3")
+        window3.setBackgroundColor(COLOR_ORANGE)
+        window2.addChildWindow(window3)
+        self.screen.addChildWindow(window1)
+        self.screen.addChildWindow(window2)
+
     
     """
     WINDOW MANAGEMENT
     """
         
     def createWindowOnScreen(self, x, y, width, height, identifier):
-        return None
+        window = Window(x, y, width, height, identifier)
+        self.screen.addChildWindow(window)
+        return window
     
     def bringWindowToFront(self, window):
-        pass
+        tempW = window.parentWindow
+        # propagate tree until top level window
+        while tempW != self.screen:
+            tempW = tempW.parentWindow
+        # tempW is screen now
+
+        # remove the parent
+        window.removeFromParentWindow()
+        # calculate new position
+        window.x, window.y = window.convertPositionToScreen(window.x, window.y)
+        # add window as top level window
+        tempW.addChildWindow(window)
+
+
+
 
     
     
@@ -32,7 +64,7 @@ class WindowSystem(GraphicsEventSystem):
     """
     
     def handlePaint(self):
-        pass
+        self.screen.draw(self.graphicsContext)
     
     
     """
@@ -40,10 +72,20 @@ class WindowSystem(GraphicsEventSystem):
     """
     
     def handleMousePressed(self, x, y):
-        pass
+        self.tempMouseDown = (x, y)
+        child = self.screen.childWindowAtLocation(x, y)
+        if child:
+            print("pressed:" + child.identifier)
+            self.bringWindowToFront(child)
+            self.handlePaint()
         
     def handleMouseReleased(self, x, y):
-        pass
+        deltaX, deltaY = abs(self.tempMouseDown[0] - x), abs(self.tempMouseDown[1] - y)
+        if deltaX <= self.mouseClickTolerance and deltaY <= self.mouseClickTolerance:
+            child = self.screen.childWindowAtLocation(x, y)
+            if child:
+                child.handleMouseClicked(x, y)
+                #self.handlePaint()
         
     def handleMouseMoved(self, x, y):
         pass
