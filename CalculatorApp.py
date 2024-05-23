9603738d18d2ffc3d3f6966d23f0acd2ede8251c
jannickbraendel <jannick2@braendel.de>
2024-05-23 13:16:14 +0200

    calc progress, adding works, still problems with typing

--- CalculatorApp.py
+++ CalculatorApp.py
@@ -1,8 +1,34 @@
+import decimal
 from functools import partial
 
 from GraphicsEventSystem import *
 from Window import *
 from UITK import *
+
+
+def floatToString(num):
+    return str(f"{num:.4f}")
+
+
+def format_number(num):
+    try:
+        dec = decimal.Decimal(num)
+    except:
+        return 'bad'
+    tup = dec.as_tuple()
+    delta = len(tup.digits) + tup.exponent
+    digits = ''.join(str(d) for d in tup.digits)
+    if delta <= 0:
+        zeros = abs(tup.exponent) - len(tup.digits)
+        val = '0.' + ('0' * zeros) + digits
+    else:
+        val = digits[:delta] + ('0' * tup.exponent) + '.' + digits[delta:]
+    val = val.rstrip('0')
+    if val[-1] == '.':
+        val = val[:-1]
+    if tup.sign:
+        return '-' + val
+    return val
 
 
 class CalculatorApp:
@@ -14,6 +40,10 @@ class CalculatorApp:
         self.windowSystem.screen.addChildWindow(self.appWindow)
 
         self.inputLabel = None
+        # temporarily stores the current result of the calculation
+        self.currentResult = 0
+        # boolean that is true if input should be overridden
+        self.overrideInput = True
         self.drawWidgets()
 
     def drawWidgets(self):
@@ -25,7 +55,7 @@ class CalculatorApp:
 
         # BUTTONS
         # array of arrays storing button labels in order of button grid layout
-        buttonLabels = [["AC", "C", "?", "/"], ["7", "8", "9", "x"], ["4", "5", "6", "-"],["1", "2", "3", "+"],
+        buttonLabels = [["AC", "C", "%", "/"], ["7", "8", "9", "x"], ["4", "5", "6", "-"], ["1", "2", "3", "+"],
                         ["+/-", "0", ".", "="]]
         buttonRowContainers = []
 
@@ -35,8 +65,10 @@ class CalculatorApp:
             # create 4 buttons for each row
             for j in range(4):
                 button = Button(0, 0, 40, 40, "button" + str(i) + str(j), LayoutAnchor.top | LayoutAnchor.left,
-                                text=buttonLabels[i][j], hoverBackgroundColor=COLOR_BLACK, pressedBackgroundColor=COLOR_ORANGE,
-                                fontColor=COLOR_WHITE, font=Font(family="Helvetica", size=14, weight=BOLD), borderColor=COLOR_BLACK)
+                                text=buttonLabels[i][j], hoverBackgroundColor=COLOR_BLACK,
+                                pressedBackgroundColor=COLOR_ORANGE,
+                                fontColor=COLOR_WHITE, font=Font(family="Helvetica", size=14, weight=BOLD),
+                                borderColor=COLOR_BLACK)
                 button.action = partial(self.handleInput, button.text)
                 if i == 0:
                     # first row
@@ -57,9 +89,9 @@ class CalculatorApp:
                 self.appWindow.addChildWindow(button)
                 buttonRow.append(button)
 
-            buttonRowContainer = Container(10, 100 + i*50, self.appWindow.width - 20, 40, "horContainer" + str(i),
-                                        layoutAnchors=LayoutAnchor.left | LayoutAnchor.top, horizontalDist=True,
-                                        containerWindows=buttonRow, spacing=10)
+            buttonRowContainer = Container(10, 100 + i * 50, self.appWindow.width - 20, 40, "horContainer" + str(i),
+                                           layoutAnchors=LayoutAnchor.left | LayoutAnchor.top, horizontalDist=True,
+                                           containerWindows=buttonRow, spacing=10)
             self.appWindow.addChildWindow(buttonRowContainer)
             buttonRowContainers.append(buttonRowContainer)
 
@@ -69,12 +101,29 @@ class CalculatorApp:
         numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
         inputText = self.inputLabel.text
         if char in numbers:
-            if inputText == "0":
+            if inputText == "0" or self.overrideInput:
                 inputText = char
+                self.overrideInput = False
             else:
                 inputText += char
+        elif char == "+":
+            inputText = self.add()
+            self.overrideInput = True
+        elif char == "-":
+            inputText = self.subtract()
+            self.overrideInput = True
+        elif char == "*":
+            inputText = self.multiply()
+            self.overrideInput = True
+        elif char == "/":
+            inputText = self.divide()
+            self.overrideInput = True
+        elif char == "%":
+            inputText = self.percent()
         elif char == "AC":
+            # reset calculator to 0
             inputText = "0"
+            self.currentResult = 0
         elif char == "C":
             if not inputText == "0":
                 if len(inputText) == 1:
@@ -82,9 +131,29 @@ class CalculatorApp:
                 else:
                     inputText = inputText[:-1]
         elif char == "=":
-            self.solve()
-
+            # display result
+            inputText = floatToString(self.currentResult)
+        inputText = format_number(round(float(inputText), 4))
         self.inputLabel.text = inputText
 
-    def solve(self):
-        pass
+    def add(self):
+        if "." in self.inputLabel.text:
+            self.currentResult += float(self.inputLabel.text)
+        else:
+            self.currentResult += int(self.inputLabel.text)
+
+        return floatToString(self.currentResult)
+
+    def subtract(self):
+        return 0
+
+    def multiply(self):
+        return 0
+
+    def divide(self):
+        return 0
+
+    def percent(self):
+        # divides current number by 100 and presents float with two
+        res = float(self.inputLabel.text) / 100
+        return floatToString(res)
